864bfd90f687120ff0607269579286b32cb52678
Your Name <rachel.barkanic@gmail.com>
2023-06-23 17:38:20 -0700

    journal only for now

--- stylegenerator/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Style
-
-
-admin.site.register(Style)
--- stylegenerator/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class StylegeneratorConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "stylegenerator"
