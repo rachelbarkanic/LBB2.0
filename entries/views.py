5c2e0af88070c149217a3d1b516c4368bf4a20ef
Your Name <rachel.barkanic@gmail.com>
2023-06-25 16:26:27 -0700

    connect form to db

--- entries/views.py
+++ entries/views.py
@@ -1,15 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Entry, Style
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
 from entries.forms import EntryForm
+from django.urls import reverse
 
 def index(request):
     entries = Entry.objects.all()
     form = EntryForm()
 
+    if request.POST:
+        entry_form = EntryForm(request.POST)
+        if entry_form.is_valid:
+            entry_form.save()
+
+            
+    # if entry.view_count is None:
+    #     entry.view_count = 1
+    # else:
+    #     entry.view_count = entry.view_count +1
+    # entry.save()
+
     context = {'entries': entries, 'form': form}
 
+
     return render(request, 'entries/index.html', context)
+
+def post_page(request, slug):
+    entry = Entry.objects.get(slug = slug)
+    form = EntryForm()
+
+    if request.POST:
+        entry_form = EntryForm(request.POST)
+        if entry_form.is_valid:
+            return HttpResponseRedirect(reverse('post_page', kwargs = {'slug': slug}))
+        else:
+            return HttpResponseRedirect(reverse('post_page', kwargs = {'slug': slug}))
+    if entry.view_count is None:
+        entry.view_count = 1
+    else:
+        entry.view_count = entry.view_count +1
+
+    entry.save()
+
+    context = {'entry': entry, 'form': form}
+
+    return render(request, 'entries/post.html', context)
+
+
+
+
 
 
 def detail(request, entry_id):
@@ -19,3 +58,7 @@ def ratings(request, entry_id):
     # results == ratings
     response = 'Looking at the ratings'
     return HttpResponse(response % entry_id)
+
+
+
+
